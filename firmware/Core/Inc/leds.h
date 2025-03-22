a3720207a598656cc6b8ab9c826e18fcec30ecce
m-guihard <maik.guihard@gmail.com>
2025-03-22 11:00:33 +0100

    LED files

---
+++ firmware/Core/Inc/leds.h
@@ -0,0 +1,14 @@
+#ifndef __LEDS_H__
+#define __LEDS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __LEDS_H__ */
---
+++ firmware/Core/Src/leds.c
@@ -0,0 +1,5 @@
+#include "leds.h"
+
+#define LEDS_NB 13
+
+uint8_t led_data[LEDS_NB][3] = {0};
